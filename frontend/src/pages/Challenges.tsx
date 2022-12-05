2e9b9f0708b5008fcd4612666b569a1944a0b9bd
wavegate <cc.frankee@gmail.com>
2022-12-04 16:48:47 -0800

    combining headlessui select with react-hook-form

--- frontend/src/pages/Challenges.tsx
+++ frontend/src/pages/Challenges.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import Challenge from "../components/Challenge";
 import useFetch from "../hooks/useFetch";
-import { Dialog } from "@headlessui/react";
+import { Dialog, Listbox } from "@headlessui/react";
+import { Controller, useController, useForm } from "react-hook-form";
 
 export type ChallengeType = {
   name: string;
@@ -13,7 +14,7 @@ export type ChallengeType = {
 };
 
 const Challenges = () => {
-  const [challenges, challengesError, challengesLoaded, challengesFetch] =
+  const [challengesResult, challengesError, challengesLoaded, challengesFetch] =
     useFetch("http://localhost:5000/challenges");
 
   useEffect(() => {
@@ -28,14 +29,36 @@ const Challenges = () => {
     challengesFetch("DELETE", { _id: _id });
   };
 
-  let [isOpen, setIsOpen] = useState(false);
+  let [isOpen, setIsOpen] = useState(true);
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    control,
+    formState: { errors },
+  } = useForm();
+
+  const ranks = [
+    "Bronze",
+    "Silver",
+    "Gold",
+    "Platinum",
+    "Diamond",
+    "Master",
+    "Grandmaster",
+  ];
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
 
   return (
     <div>
       <div className="flex flex-col mt-6">
         <div>
           {challengesLoaded ? (
-            challenges.challenges.map((challenge: ChallengeType) => {
+            challengesResult.challenges.map((challenge: ChallengeType) => {
               return (
                 <Challenge
                   challenge={challenge}
@@ -48,29 +71,59 @@ const Challenges = () => {
           )}
         </div>
         <div className="text-red-700">{challengesError}</div>
-        <button onClick={() => setIsOpen(true)}>Add new challenge</button>
         <button
-          className="border border-black p-2 rounded hover:bg-black hover:cursor-pointer hover:text-white"
+          className="border border-black p-2 rounded w-fit hover:bg-black hover:cursor-pointer hover:text-white"
+          onClick={() => setIsOpen(true)}
+        >
+          Add new challenge
+        </button>
+        <button
+          className="border border-black p-2 rounded w-fit hover:bg-black hover:cursor-pointer hover:text-white"
           onClick={createChallenge}
         >
           Create Challenge
         </button>
       </div>
-      <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
-        <Dialog.Panel>
-          <Dialog.Title>Deactivate account</Dialog.Title>
-          <Dialog.Description>
-            This will permanently deactivate your account
-          </Dialog.Description>
+      <Dialog
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        className="relative z-50"
+      >
+        <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+        <div className="fixed inset-0 flex items-center justify-center p-4">
+          <Dialog.Panel className="mx-auto max-w-sm rounded bg-white">
+            <Dialog.Title>Create challenge</Dialog.Title>
+            <Dialog.Description>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <label htmlFor="name">Name</label>
+                <input type="text" {...register("name")} />
+                <Controller
+                  control={control}
+                  name="rank"
+                  defaultValue={ranks[0]}
+                  render={({ field: { value, onChange } }) => {
+                    return (
+                      <Listbox value={value} onChange={onChange}>
+                        <Listbox.Label>Rank:</Listbox.Label>
+                        <Listbox.Button>{value}</Listbox.Button>
+                        <Listbox.Options>
+                          {ranks.map((rank, rankIndex) => (
+                            <Listbox.Option key={rankIndex} value={rank}>
+                              {rank}
+                            </Listbox.Option>
+                          ))}
+                        </Listbox.Options>
+                      </Listbox>
+                    );
+                  }}
+                />
+              </form>
+            </Dialog.Description>
 
-          <p>
-            Are you sure you want to deactivate your account? All of your data
-            will be permanently removed. This action cannot be undone.
-          </p>
-
-          <button onClick={() => setIsOpen(false)}>Deactivate</button>
-          <button onClick={() => setIsOpen(false)}>Cancel</button>
-        </Dialog.Panel>
+            <button onClick={() => setIsOpen(false)}>Deactivate</button>
+            <button onClick={() => setIsOpen(false)}>Cancel</button>
+          </Dialog.Panel>
+        </div>
       </Dialog>
     </div>
   );
